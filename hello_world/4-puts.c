11df81f935218c67c420a0df54b1f5b59da1f1bf
Cod3ron3n <thomasw96.tw@gmail.com>
2023-05-29 15:41:52 -0700

    please

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 
 /**
- * main - prints "Programmin is like building a multilingual puzzle"
+ * main - prints "Programming is like building a multilingual puzzle"
  * followed by a new line
  * Return: Always (0).
 */
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
