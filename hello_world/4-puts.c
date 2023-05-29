7ec15d391bdf0039cd2cf5911eae6cc8955443b5
Cod3ron3n <thomasw96.tw@gmail.com>
2023-05-29 15:26:31 -0700

    this time

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+# include <studio.h>
+
+/**
+ * main pritns "Programmin is like building a multilingual
+ * puzzle", followed by a new line
+ * Return: Always (0).
+*/
+
+ int main (void)
+
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
